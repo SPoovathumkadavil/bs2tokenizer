fa7d4c317727569e2d3bbe16302fb45762d3efcd
Sally <spoovathumkadavil16@claytonschools.net>
2024-09-14 15:55:50 -0500

    tk create test

--- test/src/tokenizer_test.cpp
+++ test/src/tokenizer_test.cpp
@@ -2,9 +2,16 @@
 #include <filesystem>
 #include <string>
 
+#include "tokenizer/tokenizer.hpp"
+
 TEST(LibTests, FileExistsTest)
 {
   ASSERT_TRUE(std::filesystem::exists("resources/test.txt"));
+}
+
+TEST(TokenizerTests, CreateTokenizer)
+{
+  ASSERT_NO_THROW(tokenizer{});
 }
 
 int main(int argc, char** argv)
